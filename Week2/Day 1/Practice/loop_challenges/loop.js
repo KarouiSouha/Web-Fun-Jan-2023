06fa789ba5350734e9a4d2ce67490519412e7b76
karoui souha <karoui.souha18@gmail.com>
2023-05-08 19:50:17 +0200

    commit

---
+++ Week2/Day 1/Practice/loop_challenges/loop.js
@@ -0,0 +1,30 @@
+for(var i=1;i<=20;i++){
+    if (i%2 != 0){
+        console.log(i);
+    }
+}
+
+var i=100;
+while(i>=0){
+    if(i%3 == 0){
+        console.log(i);
+    }
+    i--;
+}
+
+var arr = [4, 2.5, 1, -0.5, -2, -3.5];
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+var sum =0;
+for(var i=1;i<=100;i++){
+    sum=sum+i;
+    console.log(sum);
+}
+
+var product=1;
+for(var i=1;i<=12;i++){
+    product=product*i;
+    console.log(product);
+}
